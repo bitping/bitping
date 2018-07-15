c127dd1532a3a5da3d4cb4d853ff09a17adcbe62
Ari <ari@fullstack.io>
2018-07-15 18:33:45 -0400

    Store blocks in DB

--- cmd/ethCmd.go
+++ cmd/ethCmd.go
@@ -1,12 +1,15 @@
 package cmd
 
 import (
+	"encoding/binary"
+	"encoding/json"
 	"fmt"
 	"log"
 
 	b "github.com/auser/bitping/blockchains"
 	"github.com/auser/bitping/types"
 	"github.com/codegangsta/cli"
+	bolt "github.com/coreos/bbolt"
 )
 
 var sharedFlags = append([]cli.Flag{}, cli.StringFlag{
@@ -41,9 +44,46 @@ var EthCmd = cli.Command{
 	},
 }
 
-//
 // TODO: Break this out and make configurable
 // with config file
+
+func blockHandler(db *bolt.DB, in <-chan types.Block, errCh chan error) {
+	logger := func(block types.Block) {
+		fmt.Printf("Got a block in the block handler: %#v\n", block)
+	}
+
+	writeToDb := func(block types.Block) {
+		err := db.Update(func(tx *bolt.Tx) error {
+			b, err := tx.CreateBucketIfNotExists([]byte("blocks"))
+			if err != nil {
+				return err
+			}
+
+			id := make([]byte, 8)
+			binary.BigEndian.PutUint64(id, uint64(block.BlockNumber))
+
+			buf, err := json.Marshal(block)
+			if err != nil {
+				return err
+
+			}
+			if err := b.Put(id, buf); err != nil {
+				return err
+			}
+
+			return nil
+		})
+		if err != nil {
+			errCh <- err
+		}
+	}
+
+	for block := range in {
+		logger(block)
+		writeToDb(block)
+	}
+}
+
 func StartListening(c *cli.Context) {
 	client := makeClient(c)
 
@@ -51,14 +91,23 @@ func StartListening(c *cli.Context) {
 	var transactionCh = make(chan []types.Transaction, 16)
 	var errCh = make(chan error, 16)
 
+	// open db
+	db, err := bolt.Open("database/eth.db", 0600, nil)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer db.Close()
+
 	go client.Run(blockCh, transactionCh, errCh)
+	go blockHandler(db, blockCh, errCh)
 
 	for {
 		select {
+
 		case err := <-errCh:
 			fmt.Printf("Error listening: %#v\n", err)
-		case block := <-blockCh:
-			fmt.Printf("Got a block: %#v\n", block)
+		//case block := <-blockCh:
+		//fmt.Printf("Got a block: %#v\n", block)
 		case txs := <-transactionCh:
 			fmt.Printf("\nGot some transactions: %#v\n", txs)
 		}
